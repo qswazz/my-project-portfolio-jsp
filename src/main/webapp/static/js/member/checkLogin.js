29357d793ec87f749fb223bda868f2e1ea38ad65
qswazz <89292521+qswazz@users.noreply.github.com>
2021-10-12 00:36:45 +0900

    Feat :  추가 - Login html 파일         index html 수정
    body :
        1. index 화면 디자인 수정
        2. Login 화면 디자인 추가

--- src/main/webapp/static/js/member/checkLogin.js
+++ src/main/webapp/static/js/member/checkLogin.js
@@ -1,38 +1,87 @@
-const form = document.getElementById("login-form");
-const btnLogin = document.getElementById("btn-login");
-
-
-function checkLogin()
+function validLogin(id, pwd)
 {
-	const inputId = form.getElementById("login-id");
-	const inputPwd = form.getElementById("login-pwd");
+	const regExp = /[ㄱ-ㅎㅏ-ㅣ가-힣]/g;
 	
-	if(inputId.value == "")
+	if(id.value == "")
 	{
 		alert("아이디를 입력하세요.");
-		inputId.focus();
+		id.focus();
 		return false;
 	}
 	
-	if(inputPwd.value == "")
+	if(id.value.length < 5
+		|| regExp.test(id.value))
+	{
+		alert("유효하지 않은 아이디 형식입니다.");
+		id.focus();
+		return false;
+	}
+	
+	if(pwd.value == "")
 	{
 		alert("비밀번호를 입력하세요.");
-		inputPwd.focus();
+		pwd.focus();
 		return false;
 	}
 	
+	if(pwd.value.length < 5)
+	{
+		alert("유효하지 않은 비밀번호 형식입니다.");
+		id.focus();
+		return false;
+	}
+	
+	console.log("Success");
 	return true;
 }
 
-
-function clickLogin()
+function init()
 {
-	console.log("???");
-	if(checkLogin() == true)
-	{
-		location.href = "/member?cmd=1";
-	}
+	const btnLogin = document.getElementById("btn-login");
+
+	var id = document.getElementById("input-id");
+	var pwd = document.getElementById("input-pwd");
+
+	btnLogin.addEventListener('click', ()=>validLogin(id, pwd));
 }
 
+init();
 
-btnLogin.addEventListener('click', clickLogin);
+// const form = document.getElementById("login-form");
+// const btnLogin = document.getElementById("btn-login");
+
+
+// function checkLogin()
+// {
+// 	const inputId = form.getElementById("login-id");
+// 	const inputPwd = form.getElementById("login-pwd");
+	
+// 	if(inputId.value == "")
+// 	{
+// 		alert("아이디를 입력하세요.");
+// 		inputId.focus();
+// 		return false;
+// 	}
+	
+// 	if(inputPwd.value == "")
+// 	{
+// 		alert("비밀번호를 입력하세요.");
+// 		inputPwd.focus();
+// 		return false;
+// 	}
+	
+// 	return true;
+// }
+
+
+// function clickLogin()
+// {
+// 	console.log("???");
+// 	if(checkLogin() == true)
+// 	{
+// 		location.href = "/member?cmd=1";
+// 	}
+// }
+
+
+// btnLogin.addEventListener('click', clickLogin);
